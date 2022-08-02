88451e5aec9f3613c187088f8750d4b4dea47321
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2022-08-02 19:34:21 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
+# bash
+# alway needed
+pacman -S networkmanager gvim git cmus tmux
 
+# desktop environment for sway
+pacman -S sway qt5-wayland qt6-wayland imv mpv
+
+# programming language
+pacman -S python rust ruby dotnet-sdk jdk-openjdk
