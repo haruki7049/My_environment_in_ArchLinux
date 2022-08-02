bd772bd0081b636f27de522ebc32a29105fe530a
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2022-08-02 19:35:26 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 # alway needed
 pacman -S networkmanager gvim git cmus tmux alsa-utils
 
+# pulseaudio
+pacman -S pavucontrol
+
 # desktop environment for sway
 pacman -S sway qt5-wayland qt6-wayland imv mpv
 
